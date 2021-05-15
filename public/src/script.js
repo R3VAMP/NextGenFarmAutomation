2549f83e66fccc80ff5ade2ac427749d459f19bd
R3VAMP <saurabh.subudhiray@gmail.com>
2021-05-15 16:07:00 +0530

    changes made

--- public/src/script.js
+++ public/src/script.js
@@ -4,29 +4,29 @@ var sidebar = document.querySelector("#sidebar");
 var sidebarCloseIcon = document.querySelector("#sidebarIcon");
 
 function toggleSidebar() {
-	if (!sidebarOpen) {
-		sidebar.classList.add("sidebar_responsive");
-		sidebarOpen = true;
-	}
+  if (!sidebarOpen) {
+    sidebar.classList.add("sidebar_responsive");
+    sidebarOpen = true;
+  }
 }
 
 function closeSidebar() {
-	if (sidebarOpen) {
-		sidebar.classList.remove("sidebar_responsive");
-		sidebarOpen = false;
-	}
+  if (sidebarOpen) {
+    sidebar.classList.remove("sidebar_responsive");
+    sidebarOpen = false;
+  }
 }
 
 /* *********Firebase Connections*************** */
 
 var firebaseConfig = {
-	apiKey: "AIzaSyAKpXoGIz-h3WR4jy47b8BFhhRsgZe2EIE",
-	authDomain: "nextgenfarmautomationfinal.firebaseapp.com",
-	databaseURL: "https://nextgenfarmautomationfinal-default-rtdb.firebaseio.com",
-	projectId: "nextgenfarmautomationfinal",
-	storageBucket: "nextgenfarmautomationfinal.appspot.com",
-	messagingSenderId: "939742735387",
-	appId: "1:939742735387:web:19e8815276b36f49f99cbe",
+  apiKey: "AIzaSyAKpXoGIz-h3WR4jy47b8BFhhRsgZe2EIE",
+  authDomain: "nextgenfarmautomationfinal.firebaseapp.com",
+  databaseURL: "https://nextgenfarmautomationfinal-default-rtdb.firebaseio.com",
+  projectId: "nextgenfarmautomationfinal",
+  storageBucket: "nextgenfarmautomationfinal.appspot.com",
+  messagingSenderId: "939742735387",
+  appId: "1:939742735387:web:19e8815276b36f49f99cbe",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -40,26 +40,26 @@ firebase.initializeApp(firebaseConfig);
 // console.log(firebase);
 let x = 30;
 const showData = () => {
-	firebase
-		.database()
-		.ref()
-		.on("value", (givenName) => {
-			givenName.forEach((element) => {
-				var showTemp = element.val().tempValue;
-				var showHumid = element.val().humidityValue;
-				var showMoist = element.val().moisturePercentage;
-				var showMotor = element.val().motorStatus;
-				var showLed = element.val().ldrStatus;
-				addItem(showTemp, showHumid, showMoist, showMotor, showLed);
-			});
-		});
+  firebase
+    .database()
+    .ref()
+    .on("value", (givenName) => {
+      givenName.forEach((element) => {
+        var showTemp = element.val().tempValue;
+        var showHumid = element.val().humidityValue;
+        var showMoist = element.val().moisturePercentage;
+        var showMotor = element.val().motorStatus;
+        var showLed = element.val().ldrStatus;
+        addItem(showTemp, showHumid, showMoist, showMotor, showLed);
+      });
+    });
 };
 window.onload = showData;
 
 const addItem = (showTemp, showHumid, showMoist, showMotor, showLed) => {
-	document.querySelector("#temperature").innerText = showTemp;
-	document.querySelector("#humidity").innerText = showHumid;
-	document.querySelector("#moisture").innerText = showMoist;
-	document.querySelector("#pump-status").innerText = showMotor;
-	document.querySelector("#growing-led").innerText = showLed;
+  document.querySelector("#temperature").innerText = showTemp;
+  document.querySelector("#humidity").innerText = showHumid;
+  document.querySelector("#moisture").innerText = showMoist;
+  document.querySelector("#pump-status").innerText = showMotor;
+  document.querySelector("#growing-led").innerText = showLed;
 };
